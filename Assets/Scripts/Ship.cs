cca0669bef2f14adbc9d7cb735ba58cad941f758
Dan Newman <danlnewman@gmail.com>
2020-02-07 01:12:53 -0800

    movement test

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
+    public enum MoveDirection
+    {
+        left,
+        right,
+        up,
+        down
+    };
+
+    public MoveDirection moveDirection = MoveDirection.left;
+    public float moveStartTime;
+    public float moveEndTime;
+    public float startPosition;
+    public float endPosition;
+    public bool moving = false;
+    public float t;
+
+
     ClientMessage message;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +31,24 @@ public class Ship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!moving)
+        {
+            moveDirection = MoveDirection.left;
+            moveStartTime = Time.time;
+            moveEndTime = Time.time + 1.0f;
+            moving = true;
+            startPosition = transform.position.x;
+            endPosition = transform.position.x + 1.0f;
+        }
+
+        t = (Time.time - moveStartTime) / (moveEndTime - moveStartTime);
+        transform.position = new Vector3(Mathf.Lerp( startPosition, endPosition, t), transform.position.y, transform.position.z);
+
+        if (transform.position.x >= endPosition)
+        {
+            moving = false;
+        }
+
     }
+
 }
