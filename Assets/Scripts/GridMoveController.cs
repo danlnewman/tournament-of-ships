135566cf89d350c85bced1251e554fb43c4a1617
Dan Newman <danlnewman@gmail.com>
2020-02-08 01:47:04 -0800

    4 directions of movement

---
+++ Assets/Scripts/GridMoveController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMoveController : MonoBehaviour
+{
+    public enum MoveDirection
+    {
+        left,
+        right,
+        up,
+        down
+    };
+
+    public MoveDirection moveDirection = MoveDirection.left;
+    public float moveStartTime;
+    public float moveEndTime;
+    public float startPosition;
+    public float endPosition;
+    public bool moving = false;
+    public float t;
+    public float period = 1.0f;
+    MoveDirection[] directions = { MoveDirection.left, MoveDirection.up, MoveDirection.right, MoveDirection.down };
+    int i = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!moving)
+        {
+            moveDirection = directions[i++ % directions.Length];
+            moveStartTime = Time.time;
+            moveEndTime = Time.time + period;
+            moving = true;
+            switch (moveDirection)
+            {
+                case MoveDirection.left:
+                    startPosition = transform.position.x;
+                    endPosition = transform.position.x - 1.0f;
+                    break;
+                case MoveDirection.right:
+                    startPosition = transform.position.x;
+                    endPosition = transform.position.x + 1.0f;
+                    break;
+                case MoveDirection.up:
+                    startPosition = transform.position.y;
+                    endPosition = transform.position.y + 1.0f;
+                    break;
+                case MoveDirection.down:
+                    startPosition = transform.position.y;
+                    endPosition = transform.position.y - 1.0f;
+                    break;
+            }
+        }
+
+        t = (Time.time - moveStartTime) / (moveEndTime - moveStartTime);
+
+        switch (moveDirection)
+        {
+            case MoveDirection.left:
+                if (transform.position.x <= endPosition)
+                {
+                    moving = false;
+                }
+                transform.position = new Vector3(Mathf.Lerp(startPosition, endPosition, t), transform.position.y, transform.position.z);
+                break;
+            case MoveDirection.right:
+                if (transform.position.x >= endPosition)
+                {
+                    moving = false;
+                }
+                transform.position = new Vector3(Mathf.Lerp(startPosition, endPosition, t), transform.position.y, transform.position.z);
+                break;
+            case MoveDirection.up:
+                if (transform.position.y >= endPosition)
+                {
+                    moving = false;
+                }
+                transform.position = new Vector3(transform.position.x, Mathf.Lerp(startPosition, endPosition, t), transform.position.z);
+                break;
+            case MoveDirection.down:
+                if (transform.position.y <= endPosition)
+                {
+                    moving = false;
+                }
+                transform.position = new Vector3(transform.position.x, Mathf.Lerp(startPosition, endPosition, t), transform.position.z);
+                break;
+        }
+    }
+}
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,23 +4,6 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
-    public enum MoveDirection
-    {
-        left,
-        right,
-        up,
-        down
-    };
-
-    public MoveDirection moveDirection = MoveDirection.left;
-    public float moveStartTime;
-    public float moveEndTime;
-    public float startPosition;
-    public float endPosition;
-    public bool moving = false;
-    public float t;
-
-
     ClientMessage message;
     // Start is called before the first frame update
     void Start()
@@ -31,23 +14,6 @@ public class Ship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!moving)
-        {
-            moveDirection = MoveDirection.left;
-            moveStartTime = Time.time;
-            moveEndTime = Time.time + 1.0f;
-            moving = true;
-            startPosition = transform.position.x;
-            endPosition = transform.position.x + 1.0f;
-        }
-
-        t = (Time.time - moveStartTime) / (moveEndTime - moveStartTime);
-        transform.position = new Vector3(Mathf.Lerp( startPosition, endPosition, t), transform.position.y, transform.position.z);
-
-        if (transform.position.x >= endPosition)
-        {
-            moving = false;
-        }
 
     }
 
