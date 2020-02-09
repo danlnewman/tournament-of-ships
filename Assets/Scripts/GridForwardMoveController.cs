9d4544592c8b7355a469074043e36738017c7d91
Dan Newman <danlnewman@gmail.com>
2020-02-09 00:35:25 -0800

    partial forward move

---
+++ Assets/Scripts/GridForwardMoveController.cs
@@ -0,0 +1,36 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class GridForwardMoveController : MonoBehaviour
+//{
+//    public enum ForwardMoveDirection
+//    {
+//        left,
+//        right,
+//        forward
+//    };
+
+//    public ForwardMoveDirection moveDirection = MoveDirection.left;
+//    public float moveStartTime;
+//    public float moveEndTime;
+//    public float startPosition;
+//    public float endPosition;
+//    public bool moving = false;
+//    public float t;
+//    public float period = 1.0f;
+//    MoveDirection[] directions = { MoveDirection.left, MoveDirection.up, MoveDirection.right, MoveDirection.down };
+//    int i = 0;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+//}
