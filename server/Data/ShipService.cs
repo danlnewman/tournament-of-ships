47b5516bd04406643afcf0d9818b036544f2379f
Dan Newman <danlnewman@gmail.com>
2020-02-11 20:24:16 -0800

    add ship controller

---
+++ server/Data/ShipService.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Data
+{
+    public class ShipService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public Task<WeatherForecast[]> SendDirectionAsync(DateTime startDate)
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
---
+++ server/ShipController.cs
@@ -0,0 +1,15 @@
+// using Microsoft.AspNetCore.Mvc;
+
+// namespace server
+// {
+//     [Route("[controller]")]
+//     [ApiController]
+//     public class ShipController : Controller
+//     {
+//         [HttpGet]
+//         public ActionResult<string> Index()
+//         {
+//             return "Success";
+//         }
+//     }
+// }
