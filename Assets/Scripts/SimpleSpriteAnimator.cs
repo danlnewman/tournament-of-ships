6eaf3263258e8bc0be4e166888a0801486a9b612
Dan Newman <danlnewman@gmail.com>
2020-02-16 01:24:05 -0800

    coin animation unity

---
+++ Assets/Scripts/SimpleSpriteAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSpriteAnimator : MonoBehaviour
+{
+    [SerializeField]
+    SpriteRenderer spriteRenderer;
+    [SerializeField]
+    Sprite[] sprites;
+    [SerializeField]
+    int numberOfFrames = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRenderer.sprite = sprites[(int)(Time.time * 10f)%numberOfFrames];
+    }
+}
